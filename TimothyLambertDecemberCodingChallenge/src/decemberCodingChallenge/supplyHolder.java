4dda65ab44f0fc659b61cd8449f913b14509cec3
timothylambert <34090467+timothylambert@users.noreply.github.com>
2017-12-20 10:35:22 +0000

    Add files via upload

---
+++ TimothyLambertDecemberCodingChallenge/src/decemberCodingChallenge/supplyHolder.java
@@ -0,0 +1,35 @@
+package decemberCodingChallenge;
+
+public class supplyHolder
+{
+
+	private int xCoord;
+	private int yCoord;
+	
+	public supplyHolder(int xCoord, int yCoord)
+	{
+		this.xCoord = xCoord;
+		this.yCoord = yCoord;
+	}
+	
+	public void setXCoord(int xCoord)
+	{
+		this.xCoord = xCoord;
+	}
+	
+	public int getXCoord()
+	{
+		return xCoord;
+	}
+	
+	public void setYCoord(int yCoord)
+	{
+		this.yCoord = yCoord;
+	}
+	
+	public int getYCoord()
+	{
+		return yCoord;
+	}
+	
+}
